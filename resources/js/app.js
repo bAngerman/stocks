b133c3c98b2fe08e6df44a47c13fff318221342c
bAngerman <brendan.angerman@gmail.com>
2021-01-28 20:45:15 -0700

    test

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import Vue from 'vue'
 
 //Main pages
@@ -8,6 +7,3 @@ const root = new Vue({
     el: '#app',
     components: { App }
 });
-=======
-require('./bootstrap');
->>>>>>> 0136621843e4972d569c3efea9e10cac284fcb6c
